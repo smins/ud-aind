d09e87ccfdb1343f4986c36c752ccd2440a84160
smins <stuminshull@gmail.com>
2017-05-24 23:48:25 -0700

    Added eliminate function

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -26,24 +26,39 @@ def grid_values(grid):
 
     return grid_dict
 
-# Global vars - labels for the board
-g_rows = 'ABCDEFGHI'
-g_cols = '123456789'
+# If a box contains a single value, remove that value from all peers
+def eliminate(grid):
+
+    rows = 'ABCDEFGHI'
+    cols = '123456789'
+
+    # Generate all the needed lists
+    boxes = cross(row, cols)
+    # Unit lists
+    row_units = [cross(row, cols) for row in rows]
+    col_units = [cross(col, rows) for col in cols]
+    square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+    # Combine the units into a master list
+    unitlist = row_units + col_units + square_units
+    units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+    # Create a dict containing lists of peers for each box, keyed by box label
+    peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
+
+    # Get the list of all boxes with only 1 possible values
+    solved_vals = [box for box in grid.keys() if len(grid[box]) == 1]
+    for box in solved_values:
+        # Grab the digit of each box
+        digit = values[box]
+        for peer in peers[box]:
+            grid[peer] = grid[peer].replace(digit,'')
+
+    return grid
 
 ### Script Below ###
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
 # All boxes on the board
-boxes = cross(g_rows, g_cols)
-print(boxes)
-
-# Unit sets as lists
-row_units = [cross(row, g_cols) for row in g_rows]
-col_units = [cross(col, g_rows) for col in g_cols]
-
-# Hardcode the square units
-square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-
-# Concat all the unit sets into a master set
-unitlist = row_units + col_units + square_units
-print(unitlist)
+boxes = cross(rows, cols)
 
 print(grid_values('..0..2.1.2.2..234..21.23.2'))
